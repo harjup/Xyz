70675d2bf62a8dc3a64fc3bcbf2d6deee30cf913
harjup <pikmin@gmail.com>
2015-07-17 20:52:04 -0400

    Adjust camera movement rate to keep player closer to center, create prototype arena

--- src/Xyz/Assets/Xyz/Scripts/Camera/CameraMove.cs
+++ src/Xyz/Assets/Xyz/Scripts/Camera/CameraMove.cs
@@ -5,16 +5,19 @@ using DG.Tweening;
 
 public class CameraMove : MonoBehaviour
 {
+    // How quickly the camera accelerates to keep up with the player.
+    // Keep it under 1f.
+    public float cameraSnapX = .1f;
+    public float cameraSnapZ = .4f;
+
+    // Distance the player can move away from the camera center before it starts trying to recenter.
+    public float cameraFreeX = 2.5f;
+    public float cameraFreeZ = 1.25f;
+
+    //TODO: These should be acquired in start eventually. Bad.
     public GameObject player;
     public GameObject cameraCenter;
 
-    private Vector3 velocity = Vector3.zero;
-
-	void Start () 
-    {
-	
-	}
-	
 	private void FixedUpdate () {
         MoveCamera(cameraCenter.transform, player.transform);
 	}
@@ -23,18 +26,18 @@ public class CameraMove : MonoBehaviour
     {
         // TODO: Maybe let's use DoTween and have it be reasonably smooth instead of falling on our old iTween business
         var positionDifference = targetTransform.position - cameraTransform.position;
-        float xSpeed = Mathf.Abs(positionDifference.x) * .1f;
-        float zSpeed = Mathf.Abs(positionDifference.z) * .1f;
+        float xSpeed = Mathf.Abs(positionDifference.x) * cameraSnapX;
+        float zSpeed = Mathf.Abs(positionDifference.z) * cameraSnapZ;
 
         //Cap the camera's speed so it doesn't go fucking nuts and start overshooting the player
-        if (xSpeed > 40) { xSpeed = 40; }
-        if (zSpeed > 40) { zSpeed = 40; }
+        if (xSpeed > 60) { xSpeed = 60; }
+        if (zSpeed > 60) { zSpeed = 60; }
 
-        if (Mathf.Abs(positionDifference.x) >= 10)
+        if (Mathf.Abs(positionDifference.x) >= cameraFreeX)
         {
             cameraTransform.position = cameraTransform.position.SetX(iTween.FloatUpdate(cameraTransform.position.x, targetTransform.position.x, xSpeed));
         }
-        if (Mathf.Abs(positionDifference.z) >= 10f)
+        if (Mathf.Abs(positionDifference.z) >= cameraFreeZ)
         {
             cameraTransform.position = cameraTransform.position.SetZ(iTween.FloatUpdate(cameraTransform.position.z, targetTransform.position.z, zSpeed));
         }
