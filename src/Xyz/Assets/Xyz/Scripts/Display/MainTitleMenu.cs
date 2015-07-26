1832da6efcb23c59b00af63f63dcf5ddd6335e73
harjup <pikmin@gmail.com>
2015-07-26 16:22:29 -0400

    Add title/credits screen, add SAGDC logo to project

---
+++ src/Xyz/Assets/Xyz/Scripts/Display/MainTitleMenu.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainTitleMenu : MonoBehaviour
+{
+    private GameObject _title;
+    private GameObject _credit;
+
+    // Use this for initialization
+    void Start()
+    {
+        _title = transform.FindChild("Title").gameObject;
+        _credit = transform.FindChild("Credit").gameObject;
+
+        ShowTitle();
+    }
+
+    public void OnStartButtonClick()
+    {
+        
+    }
+
+    public void OnCreditsButtonClick()
+    {
+        ShowCredits();
+    }
+
+    public void OnBackButtonClick()
+    {
+        ShowTitle();
+    }
+
+    private void ShowTitle()
+    {
+        _title.SetActive(true);
+        _credit.SetActive(false);
+    }
+
+    private void ShowCredits()
+    {
+        _title.SetActive(false);
+        _credit.SetActive(true);
+    }
+}
