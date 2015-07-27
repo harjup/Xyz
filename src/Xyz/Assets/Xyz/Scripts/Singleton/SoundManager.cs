03586f5d743c6a2a0874300f29d91da392ef6a3a
harjup <pikmin@gmail.com>
2015-07-27 17:47:24 -0400

    Add crowd ambiance to main level

--- src/Xyz/Assets/Xyz/Scripts/Singleton/SoundManager.cs
+++ src/Xyz/Assets/Xyz/Scripts/Singleton/SoundManager.cs
@@ -5,10 +5,13 @@ public class SoundManager : Singleton<SoundManager>
 {
     // This is specifically for the main gameplay area whoop
     private AudioSource _musicSource;
+    private AudioSource _crowdSource;
     
     void Start()
     {
         _musicSource = transform.FindChild("MainMusic").GetComponent<AudioSource>();
+        _crowdSource = transform.FindChild("Crowd-Idle").GetComponent<AudioSource>();
+        _crowdSource.Play();
     }
 
     public void PlayMainTheme()
@@ -16,6 +19,7 @@ public class SoundManager : Singleton<SoundManager>
         if (!_musicSource.isPlaying)
         {
             _musicSource.Play();
+            _crowdSource.Stop();
         }
     }
 }
