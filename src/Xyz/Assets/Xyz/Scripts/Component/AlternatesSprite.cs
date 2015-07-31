32bcb16bf41762d962e21b24f1c45a236b5ba158
harjup <pikmin@gmail.com>
2015-07-31 15:54:16 -0400

    Add background image to Watch-News scene

---
+++ src/Xyz/Assets/Xyz/Scripts/Component/AlternatesSprite.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class AlternatesSprite : MonoBehaviour
+{
+    private Image _image;
+    public List<Sprite> _sprites;
+    // Use this for initialization
+    void Start()
+    {
+        _image = GetComponent<Image>();
+        StartCoroutine(DisplaySprites(_sprites));
+    }
+
+    private IEnumerator DisplaySprites(List<Sprite> sprites)
+    {
+        while (true)
+        {
+            foreach (var sprite in sprites)
+            {
+                _image.sprite = sprite;
+                yield return new WaitForSeconds(.25f);
+            }
+        }
+    }
+}
