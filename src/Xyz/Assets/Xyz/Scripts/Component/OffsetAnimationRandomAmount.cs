abbb1ce316955993a33fcff4a17825505ad7db60
harjup <pikmin@gmail.com>
2015-08-24 22:05:38 -0400

    Graphical odds and ends

---
+++ src/Xyz/Assets/Xyz/Scripts/Component/OffsetAnimationRandomAmount.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OffsetAnimationRandomAmount : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(StartWithRandomOffset());
+    }
+
+    private IEnumerator StartWithRandomOffset()
+    {
+        var animator = GetComponent<Animator>();
+        animator.speed = 0f;
+        yield return new WaitForSeconds(Random.Range(0.1f, 1f));
+        animator.speed = Random.Range(.5f, 1f);
+    }
+
+}
