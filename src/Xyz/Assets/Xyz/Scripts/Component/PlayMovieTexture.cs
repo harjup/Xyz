8a3e7ce243713ee49f18d41161d591ba04657933
harjup <pikmin@gmail.com>
2015-09-03 22:23:28 -0400

    Add Team Logo as movietexture

---
+++ src/Xyz/Assets/Xyz/Scripts/Component/PlayMovieTexture.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayMovieTexture : MonoBehaviour
+{
+    void Start()
+    {
+        var movieTexture = GetComponent<Renderer>().material.mainTexture as MovieTexture;
+
+        movieTexture.loop = true;
+        movieTexture.Play();
+    }
+}
